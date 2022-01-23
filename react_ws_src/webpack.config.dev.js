7da583de9150d0111ba2ee3c563dd873b5cbc1e7
pushbuttons <admin@pushbuttons.io>
2022-01-23 15:02:02 +1100

    Update webpack config to generate html entry point

--- react_ws_src/webpack.config.dev.js
+++ react_ws_src/webpack.config.dev.js
@@ -1,49 +1,67 @@
 var path = require('path')
 var webpack = require('webpack')
-
+var HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
-	devtool: 'cheap-module-eval-source-map',
-	// context: path.join(__dirname, 'static'),
-	entry: [
-		'eventsource-polyfill', // necessary for hot reloading with IE
-		'webpack-hot-middleware/client',
-		'./src/app'
-	],
-	output: {
-		path: path.join(__dirname, 'dist'),
-		filename: 'bundle.js',
-		// publicPath: path.join(__dirname, 'static'),
-		// publicPath: __dirname + '/static/'
-		publicPath: '/'
-	},
-	plugins: [
-		new webpack.HotModuleReplacementPlugin(),
-		new webpack.NoErrorsPlugin()
-	],
-	module: {
-		loaders: [
-			{
-				test: /\.(ico|css|gif|png|html|jpg|xml|svg)$/,
-					// loader: 'url?limit=10000',
-				loader: 'file?name=[path][name].[ext]&context=./static',
-			},
-			{
-				test: /\.jsx?/,
-				loaders: ['babel'],
-				include: path.join(__dirname, 'src')
-			},
-			{
-				test: /\.scss$/,
-				loaders: ['style', 'css', 'sass']
-			},
-			{
-				test: /\.css$/,
-				loaders: ['style', 'css']
-			},
-			{
-				test: /(flickity|fizzy-ui-utils|get-size|unipointer|imagesloaded)/,
-				loader: 'imports?define=>false&this=>window'
-			},
-		]
-	},
+    devtool: 'cheap-module-eval-source-map',
+    // context: path.join(__dirname, 'static'),
+    entry: [
+        'eventsource-polyfill', // necessary for hot reloading with IE
+        'webpack-hot-middleware/client',
+        './src/app',
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        // publicPath: path.join(__dirname, 'static'),
+        // publicPath: __dirname + '/static/',
+        publicPath: '/',
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'XXT',
+            filename: 'index.html',
+            templateContent: `<html>
+			<head>
+			<meta name="Description" content="X-ttt built with React and Webpack, React-router, Babel, Ampersand on Node.js/Express/Socket.io" />
+			<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css" />
+				<script type="text/javascript">
+					conf_file = './ws_conf.xml'
+					base_dir = '/'
+				</script>
+			</head>
+			<body>
+			<div id="root"></div>
+			</body>
+		</html>`,
+        }),
+    ],
+    module: {
+        loaders: [
+            {
+                test: /\.(ico|css|gif|png|html|jpg|xml|svg)$/,
+                // loader: 'url?limit=10000',
+                loader: 'file?name=[path][name].[ext]&context=./static',
+            },
+            {
+                test: /\.jsx?/,
+                loaders: ['babel'],
+                include: path.join(__dirname, 'src'),
+            },
+            {
+                test: /\.scss$/,
+                loaders: ['style', 'css', 'sass'],
+            },
+            {
+                test: /\.css$/,
+                loaders: ['style', 'css'],
+            },
+            {
+                test: /(flickity|fizzy-ui-utils|get-size|unipointer|imagesloaded)/,
+                loader: 'imports?define=>false&this=>window',
+            },
+        ],
+    },
 }
--- react_ws_src/webpack.config.prod.js
+++ react_ws_src/webpack.config.prod.js
@@ -1,54 +1,67 @@
 var path = require('path')
 var webpack = require('webpack')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
-
+var HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
-	devtool: 'source-map',
-	entry: [
-		'../src/app'
-	],
-	context: path.join(__dirname, 'static'),
-	output: {
-		path: path.join(__dirname, 'dist'),
-		filename: 'bundle.js',
-		publicPath: './'
-	},
-	plugins: [
-		new ExtractTextPlugin('style.css'),
-		new webpack.optimize.OccurenceOrderPlugin(),
-		new webpack.DefinePlugin({
-			'process.env': {
-				'NODE_ENV': JSON.stringify('production')
-			}
-		}),
-		new webpack.optimize.UglifyJsPlugin({
-			compressor: {
-				warnings: false
-			}
-		})
-	],
-	module: {
-		loaders: [
-			{
-				test: /\.(ico|gif|png|html|jpg|swf|xml|svg)$/,
-				loader: 'file?name=[path][name].[ext]'
-			},
-			{
-				test: /\.scss$/,
-				loader: ExtractTextPlugin.extract(
-					'style',
-					'css!sass'
-				)
-			},
-			{
-				test: /\.jsx?/,
-				loaders: ['babel'],
-				include: path.join(__dirname, 'src')
-			},
-			{
-				test: /(flickity|fizzy-ui-utils|get-size|unipointer|imagesloaded)/,
-				loader: 'imports?define=>false&this=>window'
-			},
-		]
-	},
+    devtool: 'source-map',
+    entry: ['../src/app'],
+    context: path.join(__dirname, 'static'),
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: './',
+    },
+    plugins: [
+        new ExtractTextPlugin('style.css'),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('production'),
+            },
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compressor: {
+                warnings: false,
+            },
+        }),
+        new HtmlWebpackPlugin({
+            title: 'XXT',
+            filename: 'index.html',
+            templateContent: `<html>
+			<head>
+			<meta name="Description" content="X-ttt built with React and Webpack, React-router, Babel, Ampersand on Node.js/Express/Socket.io" />
+			<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css" />
+				<script type="text/javascript">
+					conf_file = './ws_conf.xml'
+					base_dir = '/'
+				</script>
+			</head>
+			<body>
+			<div id="root"></div>
+			</body>
+		</html>`,
+        }),
+    ],
+    module: {
+        loaders: [
+            {
+                test: /\.(ico|gif|png|html|jpg|swf|xml|svg)$/,
+                loader: 'file?name=[path][name].[ext]',
+            },
+            {
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract('style', 'css!sass'),
+            },
+            {
+                test: /\.jsx?/,
+                loaders: ['babel'],
+                include: path.join(__dirname, 'src'),
+            },
+            {
+                test: /(flickity|fizzy-ui-utils|get-size|unipointer|imagesloaded)/,
+                loader: 'imports?define=>false&this=>window',
+            },
+        ],
+    },
 }
