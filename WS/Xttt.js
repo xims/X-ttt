c609e4044a77f497eeacf3c769134c805bdae157
pushbuttons <admin@pushbuttons.io>
2022-01-23 18:50:25 +1100

    cpde formatting - no chngs

--- WS/Xttt.js
+++ WS/Xttt.js
@@ -1,28 +1,27 @@
 // Setup basic express server
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-io = require('socket.io')(server);
+var express = require('express')
+var app = express()
+var server = require('http').createServer(app)
+io = require('socket.io')(server)
 
-util = require("util");							// Utility resources (logging, object inspection, etc)
+util = require('util') // Utility resources (logging, object inspection, etc)
 
 /**************************************************
-** GAME VARIABLES
-**************************************************/
-Player = require("./Player").Player;			// Player class
-players = [];									// Array of connected players
-players_avail = [];
+ ** GAME VARIABLES
+ **************************************************/
+Player = require('./Player').Player // Player class
+players = [] // Array of connected players
+players_avail = []
 
-
-var port = process.env.PORT || 3001;
+var port = process.env.PORT || 3001
 
 server.listen(port, function () {
-	console.log('Server listening at port %d', port);
-});
+    console.log('Server listening at port %d', port)
+})
 
 // Routing
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'))
 
-require('./XtttGame.js');
+require('./XtttGame.js')
 
-io.on('connection', set_game_sock_handlers);
+io.on('connection', set_game_sock_handlers)
--- react_ws_src/src/app.js
+++ react_ws_src/src/app.js
@@ -1,46 +1,54 @@
+//externals
 import React from 'react'
 import app from 'ampersand-app'
 import { render } from 'react-dom'
-import { Router, Route, Redirect, IndexRoute, browserHistory  } from 'react-router'
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 import { createHistory, useBasename } from 'history'
 import ga from 'react-ga'
 
+//styles
 import './sass/main.scss'
 
+//views
 import Main from './views/Main'
-
 import Ttt from './views/ttt/Ttt'
-
 import Txt_page from './views/pages/Txt_page'
 import PopUp_page from './views/pages/PopUp_page'
-
 import Contact from './views/pages/Contact'
 import ErrorPage from './views/pages/ErrorPage'
 
 import prep_env from './models/prep_env'
 
-
-
 let renderSite = function () {
-	return render((
-		<Router history={browserHistory}>
-			<Route path='/' component={Main}>
+    return render(
+        <Router history={browserHistory}>
+            <Route path="/" component={Main}>
+                <IndexRoute components={{ mainContent: Txt_page }} />
 
-				<IndexRoute components={{mainContent: Txt_page}} />
+                <Route
+                    path="/pg/(:page)"
+                    components={{ mainContent: Txt_page }}
+                />
 
-				<Route path='/pg/(:page)' components={{mainContent: Txt_page}} />
+                <Route path="/ttt" components={{ mainContent: Ttt }} />
 
-				<Route path='/ttt' components={{mainContent: Ttt}} />
+                <Route
+                    path="/pupg/(:pu_page)"
+                    components={{ popup: PopUp_page }}
+                />
 
-				<Route path='/pupg/(:pu_page)' components={{popup: PopUp_page}} />
+                <Route path="/contact-us" components={{ popup: Contact }} />
 
-				<Route path='/contact-us' components={{popup: Contact}} />
+                <Route
+                    path="/error/404"
+                    components={{ mainContent: ErrorPage }}
+                />
 
-				<Route path='/error/404' components={{mainContent: ErrorPage}} />
-				<Route path="*" components={{mainContent: ErrorPage}} />
-			</Route>
-		</Router>
-	), document.getElementById('root'))
+                <Route path="*" components={{ mainContent: ErrorPage }} />
+            </Route>
+        </Router>,
+        document.getElementById('root')
+    )
 }
 
 // ----------------------------------------------------------------------
@@ -50,67 +58,59 @@ let renderSite = function () {
 window.app = app
 
 app.extend({
+    settings: {
+        is_mobile: false,
+        mobile_type: null,
+        can_app: false,
+        ws_conf: null,
+        curr_user: null,
+        user_ready: false,
+        user_types: [],
+        basket_type: null,
+        basket_total: 0,
+    },
 
-	settings: {
-		is_mobile: false,
-		mobile_type: null,
-		can_app: false,
+    init() {
+        prep_env(this.start.bind(this))
+    },
 
-		ws_conf: null,
+    start_ga() {
+        ga.initialize(app.settings.ws_conf.conf.ga_acc.an, { debug: false })
+        // ga.pageview(location.pathname)
+        const loclisten = browserHistory.listen((location) => {
+            // ga.send('send', location);
+            ga.pageview(location.pathname)
+        })
+    },
 
-		curr_user: null,
+    start() {
+        const history = useBasename(createHistory)({
+            // basename: document.getElementsByTagName('base')[0] ? document.getElementsByTagName('base')[0].getAttribute('href') : ''
+            basename: base_dir,
+        })
 
-		user_ready: false,
-		user_types: [],
-		basket_type: null,
-		basket_total: 0,
+        this.start_ga()
 
-	},
+        renderSite()
+    },
 
+    show_page(u) {
+        switch (u) {
+            case 'home':
+                browserHistory.push('/')
+                break
 
-	init () {
+            default:
+                console.log('show_page event with:', u)
+                browserHistory.push(u)
+                break
+        }
+    },
 
-		prep_env(this.start.bind(this))
-
-	},
-
-	start_ga () {
-		ga.initialize(app.settings.ws_conf.conf.ga_acc.an, { debug: true });
-		// ga.pageview(location.pathname)
-		const loclisten = browserHistory.listen((location) => {
-			// ga.send('send', location);
-			ga.pageview(location.pathname)
-		})
-	},
-
-	start () {
-		const history = useBasename(createHistory)({
-			// basename: document.getElementsByTagName('base')[0] ? document.getElementsByTagName('base')[0].getAttribute('href') : ''
-			basename: base_dir
-		})
-
-		this.start_ga()
-
-		renderSite()
-	},
-
-	show_page (u) {
-		switch(u) {
-			case 'home':
-				browserHistory.push('/')
-				break
-
-			default:
-				console.log('show_page event with:', u) 
-				browserHistory.push(u)
-				break
-		}
-	},
-
-	events: {
-		show_message: 'show_message',
-		show_page: 'show_page'
-	},
+    events: {
+        show_message: 'show_message',
+        show_page: 'show_page',
+    },
 })
 
 app.init()
