91e96e6b2dc7276cba1e773c8bd52473e4239959
Frank Pethica <frank@getbamboo.io>
2022-05-13 06:36:45 +0800

    minor css changes layout behaves down to width 700

--- react_ws_src/src/views/layouts/Footer.js
+++ react_ws_src/src/views/layouts/Footer.js
@@ -8,7 +8,10 @@ export default class Footer extends Component {
 		return (
 			<footer>
 				<div className='container'>
-					<nav>
+
+          <div className='foot_message'> {app.settings.ws_conf.footer.foot_msg.txt} </div>
+        
+          <nav>
 						<ul>
 							{
 								app.settings.ws_conf.footer.items.i.map(function (it, i) {
@@ -21,8 +24,6 @@ export default class Footer extends Component {
 							}
 						</ul>
 					</nav>
-
-					<div className='foot_message'> {app.settings.ws_conf.footer.foot_msg.txt} </div>
 
 					<a className='foot-r-logo' href={app.settings.ws_conf.footer.foot_r_logo.u} target={app.settings.ws_conf.footer.foot_r_logo.t} rel="noopener noreferrer" >
 						<img alt='footer logo' src={app.settings.ws_conf.footer.foot_r_logo.i} />
