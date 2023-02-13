9835d5c53161a55c6400d951eecd92470464714c
Michael Worobec <eekumu1@hotmail.com>
2023-02-14 08:57:16 +1100

    use utility to space buttons + tidy up

--- react_ws_src/src/views/ttt/SetGameType.js
+++ react_ws_src/src/views/ttt/SetGameType.js
@@ -1,51 +1,26 @@
-import React, {Component} from 'react'
-
+import React from 'react'
 import Button from '../../components/Button'
 
-export default class SetGameType extends Component {
-
-	constructor (props) {
-		super(props)
-
-		this.state = {}
+export default function SetGameType({ onSetType }) {
+	function selTypeLive() {
+		onSetType('live')
 	}
 
-//	------------------------	------------------------	------------------------
+	function selTypeComp() {
+		onSetType('comp')
+	}
 
-	render () {
-		return (
-			<div id='SetGameType'>
-
-				<h1>Choose game type</h1>
-
-				<Button long onClick={this.selTypeLive.bind(this)}>Live against another player</Button>
-				
-				&nbsp;&nbsp;&nbsp;&nbsp;
-
-				<Button long onClick={this.selTypeComp.bind(this)}>Against a computer</Button>
-
+	return (
+		<div id="SetGameType">
+			<h1>Choose game type</h1>
+			<div className="space-x-3">
+				<Button long onClick={selTypeLive}>
+					Live against another player
+				</Button>
+				<Button long onClick={selTypeComp}>
+					Against a computer
+				</Button>
 			</div>
-		)
-	}
-
-//	------------------------	------------------------	------------------------
-
-	selTypeLive (e) {
-		// const { name } = this.refs
-		// const { onSetType } = this.props
-		// onSetType(name.value.trim())
-
-		this.props.onSetType('live')
-	}
-
-//	------------------------	------------------------	------------------------
-
-	selTypeComp (e) {
-		// const { name } = this.refs
-		// const { onSetType } = this.props
-		// onSetType(name.value.trim())
-
-		this.props.onSetType('comp')
-	}
-
+		</div>
+	)
 }
