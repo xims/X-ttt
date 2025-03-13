aa87b3c686ea83e9d5ab9355432975326e313ef1
Moditha Akalanka <randhiron@gmail.com>
2025-03-13 17:24:42 +1100

    Refactor static file serving in devServer.js

--- react_ws_src/devServer.js
+++ react_ws_src/devServer.js
@@ -13,14 +13,8 @@ app.use(require('webpack-dev-middleware')(compiler, {
 
 app.use(require('webpack-hot-middleware')(compiler));
 
-// app.use(express.static(paths.client('images')))
-// app.use(express.static('static'))
-
-var proxy = require('proxy-middleware');
-var url = require('url');
-// app.use('/images', proxy(url.parse('../WS/images')));
-// app.use('/img', proxy(url.parse('../WS/img')));
-app.use('/images', proxy(url.parse('http://z2/projs/kisla/X-react-starter/dev/WS/images')));
+// Serve static files
+app.use(express.static(path.join(__dirname, 'static')));
 
 app.get('*', function(req, res) {
 	res.sendFile(path.join(__dirname, 'static', 'index.html'));
